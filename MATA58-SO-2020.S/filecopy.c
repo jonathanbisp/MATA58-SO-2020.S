8a56058c4b3d36c691ed143ca9418d16d1831405
Jonathan Bispo <jonathan.bispo@ufba.br>
2020-09-19 20:41:05 -0300

    Add files via upload

--- MATA58-SO-2020.S/filecopy.c
+++ MATA58-SO-2020.S/filecopy.c
@@ -8,26 +8,24 @@ char *strerror(int errnum);
 
 int main (int * argc, char * argv[]){ 
 
-
     if( argc != 3 ){
       printf("Quantidade de argumentos inválida.");
       return 1;
     }
 
-    int filecopied = open(argv[1], O_RDONLY);
+    int filecopied = open(argv[1], O_RDONLY, 0755);
     //Verifica se o arquivo a copiar existe
     if(filecopied < 0) {
-      printf("ERRO: Não foi possível abrir o código %s: %s  \n", argv[1], strerror(errno));
+      printf("filecopy: Não foi possível abrir o arquivo %s: %s  \n", argv[1], strerror(errno));
 
       return 1;
     }
     
     //abrir arquivo se o arquivo não existir cria o arquivo
-    int filetarget = open(argv[2], O_RDWR | O_CREAT); 
+    int filetarget = open(argv[2], O_RDWR | O_CREAT, 0755); 
     //Verifica se a ação de abrir ou criar ocorreu com sucesso
     if(filetarget < 0){
-      printf("ERRO: A ação não foi concluída: %s \n", strerror(errno) );
-
+      printf("filecopy: Não foi possível abrir ou criar o arquivo: %s \n", strerror(errno) );
       return 1;
     }
     char buffer[4096];
@@ -35,8 +33,6 @@ int main (int * argc, char * argv[]){
 
     /* read write */
       while(1){
-        int offset = 1;
-        
 
         //read retorna sempre um valor; o valor do buffer,a quantidade lida, e 0 endoffile
         int rbytes = read(filecopied, buffer, sizeof(buffer));
@@ -55,7 +51,14 @@ int main (int * argc, char * argv[]){
           printf("Não foi possivel escrever o arquivo: %s \n", strerror(errno));
         }
       }
-  
-  close(filecopied);
-  close(filetarget);
+  int copiedClosed = close(filecopied);
+    if(copiedClosed < 0){
+      printf("Não foi possível fechar o arquivo copiado: %s \n", strerror(errno));
+    }
+  int targetClosed = close(filetarget);
+    if(targetClosed < 0){
+        printf("Não foi possível fechar a cópia do arquivo original: %s \n", strerror(errno));
+    }
+
+  printf("filecopy: foram copiados %d bytes do arquivo %s para o arquivo %s \n",totalbytes, argv[1], argv[2]);
 }
